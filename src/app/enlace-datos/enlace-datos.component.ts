cb31c8c2456333812278a8599a1853e5aaccf703
alexun <alexunjm@gmail.com>
2017-02-12 15:35:36 -0500

    enlace de datos

--- src/app/enlace-datos/enlace-datos.component.ts
+++ src/app/enlace-datos/enlace-datos.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-enlace-datos',
   templateUrl: './enlace-datos.component.html',
   styleUrls: ['./enlace-datos.component.css']
 })
-export class EnlaceDatosComponent implements OnInit {
+export class EnlaceDatosComponent {
 
   stringVar = 'así se pasan datos desde el componente';
   numberVar = 2;
 
-  constructor() { }
-
-  ngOnInit() {
+  onTest() {
+    return true;
   }
 
 }
